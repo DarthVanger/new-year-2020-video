10f3b726571f2a0947760e45ee4656707a0a538e
DarthVanger <darthvanger@gmail.com>
2021-01-01 21:40:21 +0200

    Disable images :)

--- render.sh
+++ render.sh
@@ -1,7 +1,7 @@
 source ~/.bash_profile
 
-#sh cut.sh --preview
-#sh merge.sh
-sh images.sh
-#vlc output/video.mp4
-vlc output/video_t.mp4
+sh cut.sh --preview
+sh merge.sh
+#sh images.sh
+vlc output/video.mp4
+#vlc output/video_t.mp4
