a9f2ba2434c148fff16101d14bde5ec7f427ad07
DarthVanger <darthvanger@gmail.com>
2021-01-01 18:53:25 +0200

    Improve 1st part cut

--- cut.sh
+++ cut.sh
@@ -17,7 +17,7 @@ case "$1" in
 
 echo "Cutting files from '${source_folder}'"
 
-ffmpeg -y -ss 00:00:00 -i "${source_folder}/1.mp4"  -t 99 "${output_folder}/01.mp4"
+ffmpeg -y -ss 00:00:00 -i "${source_folder}/1.mp4"  -t 94 "${output_folder}/01.mp4"
 ffmpeg -y -ss 00:02:51 -i "${source_folder}/1.mp4"  -t 4 "${output_folder}/02.mp4"
 ffmpeg -y -ss 00:00:02.5 -i "${source_folder}/2.mp4"  -t 7 "${output_folder}/03.mp4"
 ffmpeg -y -ss 00:00:21 -i "${source_folder}/2.mp4"  -t 10.5 "${output_folder}/04.mp4"
