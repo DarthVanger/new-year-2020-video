171e61d0a9621b695f53f31a04a42d6febe3d9c3
DarthVanger <darthvanger@gmail.com>
2021-01-01 21:34:27 +0200

    Add images v0.1

--- images.sh
+++ images.sh
@@ -1,9 +1,23 @@
 # add images
-mv output/video.mp4 output/temp1.mp4
-convert images/bezviz.jpg -resize 480x270 output/bezviz.jpg
+#convert images/bezviz.jpg -resize 480x270 output/bezviz.jpg
+convert images/bandera.jpg -resize 480x270 output/bandera.jpg
+convert images/ussr.jpg -resize 480x270 output/ussr.jpg
+#ffmpeg -y -i images/grad.mp4 -vf "scale=470:270" output/grad.mp4
 
-ffmpeg -y -i  output/temp1.mp4 -i output/bezviz.jpg \
-    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,115,115.5)'" \
-     output/video.mp4
+ffmpeg -y -i  output/video.mp4 -i output/bezviz.jpg \
+    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,122.25,122.5)'" \
+     output/temp2.mp4
+
+ffmpeg -y -i output/temp2.mp4 -i output/grad.mp4 \
+    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,122.5,122.75)'" \
+     output/temp3.mp4
+
+ffmpeg -y -i output/temp3.mp4 -i output/ussr.jpg \
+    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,123,123.25)'" \
+     output/temp4.mp4
+
+ffmpeg -y -i output/temp4.mp4 -i output/bandera.jpg \
+    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,123.25,123.5)'" \
+     output/video_t.mp4
 
 #    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,104,104.5)'" \
