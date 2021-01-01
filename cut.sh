683260d86e92e35b57e83fb2cfaa8acae65c8edd
DarthVanger <darthvanger@gmail.com>
2021-01-01 18:33:58 +0200

    Cut the 2nd and the 3rd parts

--- cut.sh
+++ cut.sh
@@ -19,3 +19,7 @@ echo "Cutting files from '${source_folder}'"
 
 ffmpeg -y -ss 00:00:00 -i "${source_folder}/1.mp4"  -t 99 "${output_folder}/01.mp4"
 ffmpeg -y -ss 00:02:51 -i "${source_folder}/1.mp4"  -t 4 "${output_folder}/02.mp4"
+ffmpeg -y -ss 00:00:02.5 -i "${source_folder}/2.mp4"  -t 7 "${output_folder}/03.mp4"
+ffmpeg -y -ss 00:00:21 -i "${source_folder}/2.mp4"  -t 10.5 "${output_folder}/04.mp4"
+ffmpeg -y -ss 00:00:00 -i "${source_folder}/3.mp4"  -t 4 "${output_folder}/05.mp4"
+ffmpeg -y -ss 00:00:37.5 -i "${source_folder}/3.mp4"  -t 23.5 "${output_folder}/06.mp4"
