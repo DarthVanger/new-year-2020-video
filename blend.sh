8699d68f38e732b145953e5bd4e811c372ee2601
DarthVanger <darthvanger@gmail.com>
2021-01-02 18:49:55 +0200

    Support non-preview mode in blend.sh

--- blend.sh
+++ blend.sh
@@ -1,3 +1,8 @@
+case "$1" in
+   --preview) source_folder="output/preview" ;;
+           *) source_folder="source_video" ;;
+     esac
+
 ffmpeg -y  -i output/video.mp4 -t 120 output/01.mp4
 ffmpeg -y -ss 120 -i output/video.mp4 -t 10 output/04.mp4
 ffmpeg -y -ss 130 -i output/video.mp4 -t 5 output/05.mp4
@@ -5,14 +10,13 @@ ffmpeg -y -ss 135 -i output/video.mp4 -t 5 output/07.mp4
 ffmpeg -y -ss 140 -i output/video.mp4 -t 10 output/08.mp4
 ffmpeg -y -ss 150 -i output/video.mp4 output/09.mp4
 
-ffmpeg -y -ss 10 -i source_video/shedryk.mp4 -t 8 output/shedryk.mp4
-ffmpeg -y -i output/preview/berlin-cut.mp4 -t 10 -vf 'fade=t=in:st=0:d=10' output/berlin.mp4
-ffmpeg -y -i output/preview/red-army-cut.mp4 -t 5 output/red-army.mp4
-ffmpeg -y -ss 67 -i output/preview/weed.mp4 -t 5 output/weed.mp4
-ffmpeg -y -ss 22 -i output/preview/berlin.mp4 -t 5 output/berlin2.mp4
-#ffmpeg -y -ss 349 -i output/preview/maidan.mp4 -t 10 output/maidan.mp4
-ffmpeg -y -ss 121 -i output/preview/maidan.mp4 -t 10 output/maidan.mp4
-ffmpeg -y -ss 65 -i source_video/shedryk.mp4 -t 14 output/shedryk2.mp4
+ffmpeg -y -ss 10 -i $source_folder/shedryk.mp4 -t 8 output/shedryk.mp4
+ffmpeg -y -i $source_folder/berlin-cut.mp4 -t 10 -vf 'fade=t=in:st=0:d=10' output/berlin.mp4
+ffmpeg -y -i $source_folder/red-army-cut.mp4 -t 5 output/red-army.mp4
+ffmpeg -y -ss 67 -i $source_folder/weed.mp4 -t 5 output/weed.mp4
+ffmpeg -y -ss 22 -i $source_folder/berlin.mp4 -t 5 output/berlin2.mp4
+ffmpeg -y -ss 121 -i $source_folder/maidan.mp4 -t 10 output/maidan.mp4
+ffmpeg -y -ss 65 -i $source_folder/shedryk.mp4 -t 14 output/shedryk2.mp4
 
 ffmpeg -y -i output/04.mp4 -i output/berlin.mp4 \
     -filter_complex "[0:v] format=rgba [a]; [1:v] format=rgba [b]; [a][b] blend=all_expr='A*if(gte(X,440/2.66), 1, 0)*if(lte(X,840/2.66), 1, 0) + B*if(gte(X,440/2.66), 0, 1) + B*if(lte(X,840/2.66), 0, 1)'" \
--- render.sh
+++ render.sh
@@ -2,6 +2,6 @@ source ~/.bash_profile
 
 sh cut.sh --preview
 sh merge.sh
-#sh images.sh
-vlc output/video.mp4
-#vlc output/video_t.mp4
+sh blend.sh --preview
+#vlc output/video.mp4
+vlc output/video_t.mp4
