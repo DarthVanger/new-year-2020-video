9da0968f97f1f05b8e37df89cef4d5421f649adf
DarthVanger <darthvanger@gmail.com>
2021-01-02 02:04:49 +0200

    Add the last blend :)

--- blend.sh
+++ blend.sh
@@ -1,19 +1,21 @@
-ffmpeg -y  -i output/video.mp4  -t 98 output/01.mp4
-ffmpeg -y -ss 98 -i output/video.mp4  -t 17 output/02.mp4
+ffmpeg -y  -i output/video.mp4 -t 107 output/01.mp4
+ffmpeg -y -ss 107 -i output/video.mp4 -t 8 output/02.mp4
 ffmpeg -y -ss 115 -i output/video.mp4 -t 5 output/03.mp4
 ffmpeg -y -ss 120 -i output/video.mp4 -t 5 output/04.mp4
 ffmpeg -y -ss 125 -i output/video.mp4 -t 5 output/05.mp4
 ffmpeg -y -ss 130 -i output/video.mp4 -t 5 output/06.mp4
 ffmpeg -y -ss 135 -i output/video.mp4 -t 5 output/07.mp4
-ffmpeg -y -ss 140 -i output/video.mp4 output/08.mp4
+ffmpeg -y -ss 140 -i output/video.mp4 -t 10 output/08.mp4
+ffmpeg -y -ss 150 -i output/video.mp4 output/09.mp4
 
-ffmpeg -y -ss 6 -i source_video/shedryk.mp4 -t 17 output/shedryk.mp4
-ffmpeg -y  -i output/preview/grad.mp4 -t 5 output/grad.mp4
-ffmpeg -y  -i output/preview/berlin-cut.mp4 -t 5 output/berlin.mp4
-ffmpeg -y  -i output/preview/red-army-cut.mp4 -t 5 output/red-army.mp4
-ffmpeg -y  -ss 90 -i output/preview/church.mp4 -t 5 output/church.mp4
-ffmpeg -y  -ss 22 -i output/preview/berlin.mp4 -t 5 output/berlin2.mp4
-ffmpeg -y  -ss 349 -i output/preview/maidan.mp4 -t 10 output/maidan.mp4
+ffmpeg -y -ss 10 -i source_video/shedryk.mp4 -t 8 output/shedryk.mp4
+ffmpeg -y -i output/preview/grad.mp4 -t 5 output/grad.mp4
+ffmpeg -y -i output/preview/berlin-cut.mp4 -t 5 output/berlin.mp4
+ffmpeg -y -i output/preview/red-army-cut.mp4 -t 5 output/red-army.mp4
+ffmpeg -y -ss 90 -i output/preview/church.mp4 -t 5 output/church.mp4
+ffmpeg -y -ss 22 -i output/preview/berlin.mp4 -t 5 output/berlin2.mp4
+ffmpeg -y -ss 349 -i output/preview/maidan.mp4 -t 10 output/maidan.mp4
+ffmpeg -y -ss 57 -i source_video/shedryk.mp4 -t 22 output/shedryk2.mp4
 
 ffmpeg -y -i output/02.mp4 -i output/shedryk.mp4 \
     -filter_complex "[0:v][1:v] blend=all_mode='overlay'" \
@@ -25,12 +27,10 @@ ffmpeg -y -i output/b01-1.mp4 -i output/shedryk.mp4 \
 
 ffmpeg -y -i output/03.mp4 -i output/grad.mp4 \
     -filter_complex "[0:v][1:v] blend=all_mode='overlay'" \
-    -shortest \
     output/b02-1.mp4
 
 ffmpeg -y -i output/b02-1.mp4 -i output/grad.mp4 \
     -filter_complex "[0:v] format=rgba [a]; [1:v] format=rgba [b]; [a][b] blend=all_expr='A*if(gte(X,440/2.66), 1, 0)*if(lte(X,840/2.66), 1, 0) + B*if(gte(X,440/2.66), 0, 1) + B*if(lte(X,840/2.66), 0, 1)'" \
-    -shortest \
     output/b02.mp4
 
 
@@ -53,7 +53,7 @@ ffmpeg -y -i output/b04-1.mp4 -i output/red-army.mp4 \
 
 
 ffmpeg -y -i output/06.mp4 -i output/church.mp4 \
-    -filter_complex "[0:v][1:v] blend=all_mode='overlay'" \
+    -filter_complex "[0:v][1:v] blend=all_mode='overlay':all_opacity=0.5" \
     output/b05-1.mp4
 
 ffmpeg -y -i output/b05-1.mp4 -i output/church.mp4 \
@@ -77,5 +77,15 @@ ffmpeg -y -i output/08.mp4 -i output/maidan.mp4 \
 ffmpeg -y -i output/b07-1.mp4 -i output/maidan.mp4 \
     -filter_complex "[0:v] format=rgba [a]; [1:v] format=rgba [b]; [a][b] blend=all_expr='A*if(gte(X,440/2.66), 1, 0)*if(lte(X,840/2.66), 1, 0) + B*if(gte(X,440/2.66), 0, 1) + B*if(lte(X,840/2.66), 0, 1)'" \
     output/b07.mp4
+
+
+ffmpeg -y -i output/09.mp4 -i output/shedryk2.mp4 \
+    -filter_complex "[0:v][1:v] blend=all_mode='overlay'" \
+    output/b08-1.mp4
+
+ffmpeg -y -i output/b08-1.mp4 -i output/shedryk2.mp4 \
+    -filter_complex "[0:v] format=rgba [a]; [1:v] format=rgba [b]; [a][b] blend=all_expr='A*if(gte(X,440/2.66), 1, 0)*if(lte(X,840/2.66), 1, 0) + B*if(gte(X,440/2.66), 0, 1) + B*if(lte(X,840/2.66), 0, 1)'" \
+    output/b08.mp4
+
 
 ffmpeg -y -f concat -safe 0 -i blend-concat.txt -c copy output/video_t.mp4
--- cut.sh
+++ cut.sh
@@ -24,5 +24,5 @@ ffmpeg -y -ss 00:00:21 -i "${source_folder}/2pad.mp4"  -t 10.5 "${output_folder}
 ffmpeg -y -ss 00:00:02.5 -i "${source_folder}/3pad.mp4"  -t 7 "${output_folder}/05.mp4"
 ffmpeg -y -ss 00:00:22 -i "${source_folder}/3pad.mp4"  "${output_folder}/06.mp4"
 ffmpeg -y -ss 00:00:00 -i "${source_folder}/4pad.mp4"  -t 4 "${output_folder}/07.mp4"
-ffmpeg -y -ss 00:00:37.5 -i "${source_folder}/4pad.mp4"  -t 23.5 "${output_folder}/08.mp4"
+ffmpeg -y -ss 00:00:50 -i "${source_folder}/4pad.mp4"  -t 11 "${output_folder}/08.mp4"
 ffmpeg -y -ss 00:00:00 -i "${source_folder}/5pad.mp4"  -t 21.2 "${output_folder}/09.mp4"
